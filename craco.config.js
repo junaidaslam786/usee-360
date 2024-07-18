5335f8f8b2091649fee9b524b7b048d76f97f518
Muhammad Junaid Aslam <15-10648@formanite.fccollege.edu.pk>
2024-07-18 16:56:38 +0500

    Refactor code to handle parsing errors and improve error handling in agent analytics and also fix the process.env error

--- craco.config.js
+++ craco.config.js
@@ -1,15 +1,37 @@
+
+
+// const webpack = require('webpack');
 // const path = require('path');
 
 // module.exports = {
 //   webpack: {
 //     configure: (webpackConfig, { env, paths }) => {
+//       // Define environment variables
+//       const envVariables = {
+//         'process.env': JSON.stringify(process.env),
+//         'process.platform': JSON.stringify('browser'),
+//         'process.browser': JSON.stringify(true)
+//       };
+
+//       // Add DefinePlugin only once
+//       webpackConfig.plugins.push(new webpack.DefinePlugin(envVariables));
+
+//       // Add ProvidePlugin for process
+//       webpackConfig.plugins.push(new webpack.ProvidePlugin({
+//         process: 'process/browser', // This automatically loads when 'process' is used
+//       }));
+
+//       // Configure fallbacks for Node.js core modules and process
 //       webpackConfig.resolve.fallback = {
-//         fs: false, // Most web browsers don't have the 'fs' module
+//         fs: false,
 //         buffer: require.resolve('buffer/'),
 //         crypto: require.resolve('crypto-browserify'),
 //         util: require.resolve('util/'),
-//         stream: require.resolve('stream-browserify')
+//         stream: require.resolve('stream-browserify'),
+//         process: require.resolve('process/browser'),
+//         vm: require.resolve('vm-browserify')
 //       };
+
 //       return webpackConfig;
 //     }
 //   }
@@ -22,13 +44,16 @@ module.exports = {
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
       // Define environment variables
-      const envVariables = {
-        'process.env': JSON.stringify(process.env),
-        'process.platform': JSON.stringify('browser'),
-        'process.browser': JSON.stringify(true)
-      };
+      const envVariables = Object.keys(process.env).reduce((acc, key) => {
+        acc[`process.env.${key}`] = JSON.stringify(process.env[key]);
+        return acc;
+      }, {});
 
-      // Add DefinePlugin only once
+      envVariables['process.platform'] = JSON.stringify('browser');
+      envVariables['process.browser'] = JSON.stringify(true);
+
+      // Add DefinePlugin only once, ensuring no conflicting definitions
+      webpackConfig.plugins = webpackConfig.plugins.filter(plugin => !(plugin instanceof webpack.DefinePlugin));
       webpackConfig.plugins.push(new webpack.DefinePlugin(envVariables));
 
       // Add ProvidePlugin for process
